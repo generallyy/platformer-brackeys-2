9c19bff518dd1e440693da01c5744525d5f86dd8
aedan w. <aedan.wen@gmail.com>
2026-04-07 15:41:55 -0400

    level transition from lv1 to lv0
    claudeless. because it failed me.

--- scripts/LevelTransition.gd
+++ scripts/LevelTransition.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 var _triggered := false
+@export_file("*.tscn") var target_level_path: String
 
 func _on_body_entered(body) -> void:
 	if _triggered:
@@ -9,9 +10,12 @@ func _on_body_entered(body) -> void:
 		return
 	if NetworkManager.is_active() and not multiplayer.is_server():
 		return
+	if target_level_path == "":
+			print("No level path set!")
+			return
 	_triggered = true
 	var main = get_tree().get_root().get_node("Main")
 	if NetworkManager.is_active():
-		main.load_level.rpc("res://scenes/levels/Level1.tscn")
+		main.load_level.rpc(target_level_path)
 	else:
-		await main.load_level("res://scenes/levels/Level1.tscn")
+		await main.load_level(target_level_path)
